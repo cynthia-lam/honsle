38b0e832a7d9ee2ac52b00302de6eb2d58e01a93
cynthia-lam <77031089+cynthia-lam@users.noreply.github.com>
2024-09-17 15:56:58 +0000

    Add tryLimit, display()

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import Input from './Input';
 import GuessList from './GuessList';
 
 function App() {
+  const tryLimit = 6;
   const [tryNumber, setTryNumber] = useState(0);
+
+  function display() {
+    while (tryNumber > tryLimit) {
+      return (
+        <GuessList/>
+      )
+    }
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <GuessList/>
+        {display()}
         <Input/>
       </header>
     </div>
@@ -19,3 +28,16 @@ function App() {
 }
 
 export default App;
+
+// planning
+/* 
+- state/global vars: tryLimit, tryNumber
+- flow:
+  - guess at bottom
+  - onSubmit: tryNumber ++, populate <GuessList/> with <GuessLetter/> mapped through currentGuess
+  - assign colours based on rules
+- functions:
+  - display: keep on running while tryNumber <= try Limit
+  - letterChecker: assigns styling based on letter
+  - openModal: when gameOver
+*/
