cfb8937220139da789e9cf7c5a65368b85c308bd
cynthia-lam <77031089+cynthia-lam@users.noreply.github.com>
2024-09-17 17:12:51 +0000

    Update guesses to always have 6 li but update each element at corresponding tryNumber

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import GuessList from './GuessList';
 
 function App() {
   const tryLimit = 6;
+  const answer = "honse"; //change this to get random word from API call in the future
   const [tryNumber, setTryNumber] = useState(0);
-  const [guesses, setGuesses] = useState([]);
+  const [guesses, setGuesses] = useState(["     ", "     ", "     ", "     ", "     ", "     "]);
 
   function handleSubmit(e) {
     e.preventDefault();
     const guess = e.target.elements[0].value;
     if (tryNumber < tryLimit) {
-      setGuesses([...guesses, guess]); // Add new guess to the array
+      const newGuesses = [...guesses];
+      newGuesses[tryNumber] = guess;
+      setGuesses(newGuesses); // Add new guess to the array
       setTryNumber(tryNumber + 1);
-      e.target.reset(); // Optional: clear the input field after submission
+      e.target.reset(); // Clear the input field after submission
     }
   };
 
