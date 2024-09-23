7033a5f88ba49653fc63f159107d1c1587e90f6e
cynthia-lam <77031089+cynthia-lam@users.noreply.github.com>
2024-09-23 19:01:07 +0000

    Add conditional styling through function colour that descides what colour each GuessLetter should be

--- src/GuessLetter.jsx
+++ src/GuessLetter.jsx
@@ -1,9 +1,34 @@
 import "./styles/GuessLetter.css"
 
-function GuessLetter (props) {
+function GuessLetter(props) {
   const letter = props.letter;
+  const index = props.index;
+  console.log(`index is: ${index}`);
+  
+
+  function colour(letter, index) {
+    console.log(`index: ${index}, letter: ${letter}`);
+    
+    switch (true) {
+      case index === 0 && letter === 'h':
+        return 'green';
+        console.log("index is 0 AND letter is h");
+      case index === 1 && letter === 'o':
+        return 'green';
+      case index === 2 && letter === 'n':
+        return 'green';
+      case index === 3 && letter === 's':
+        return 'green';
+      case index === 4 && letter === 'e':
+      case 'honse'.includes(letter):
+        return 'yellow';
+      default:
+        return 'black';
+    }
+  }
+
   return (
-    <div className="guessLetter">
+    <div className={`guessLetter ${colour(letter, index)}`}>
       {letter}
     </div>
   )
